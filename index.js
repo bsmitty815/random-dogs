e2344fd7a9ff26e3634880db93ee89848b557126
Brian Smith <brian.bubbam@gmail.com>
2021-06-07 12:14:49 -0500

    setting up html file, and adding the dog image to the html

--- index.js
+++ index.js
@@ -4,4 +4,19 @@
 
 fetch('https://dog.ceo/api/breeds/image/random')
 .then(response => response.json())
-.then(data => console.log(data))
+.then(randomDog =>  {
+    renderDog(randomDog)
+})
+.catch(error => {
+    console.log(error)
+})
+
+function renderDog(randomDog) {
+    console.log(randomDog)
+    const div = document.getElementById('random-dog-image')
+    const ul = document.createElement('ul')
+    const dogImage = document.createElement('img')
+    dogImage.src = randomDog.message
+    div.appendChild(ul)
+    ul.appendChild(dogImage)
+}
