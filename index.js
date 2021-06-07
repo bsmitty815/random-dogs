79d4284f7ce69e278b6ca59622d6c973608b5c02
Brian Smith <brian.bubbam@gmail.com>
2021-06-07 17:01:22 -0500

    fixed save dog feature with buttons and images. Updated some of the css for the save fave dogs div.

--- index.js
+++ index.js
@@ -18,23 +18,21 @@ fetch('https://dog.ceo/api/breeds/image/random')
 
 fetchRandomDog()
 
-function renderDog(randomDog) {
-    //console.log(randomDog)
+function renderDog(randomDog) { //this adds a random dog to div random dog image
     const div = document.getElementById('random-dog-image')
     div.innerHTML = '';
     const span = document.createElement('span')
     const dogImage = document.createElement('img')
-    // need to create image size no more then certain width
     dogImage.src = randomDog.message
     div.appendChild(span)
     span.appendChild(dogImage)
 
 }
 
-//newDogButton.addEventListener('click', getNewDog);//runs the function of creating a new dog again
 
 
-function getNewDog() {
+
+function getNewDog() { //this fetches the new dog once new dog button is clicked
     fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
     .then(randomDog =>  {
@@ -46,25 +44,20 @@ function getNewDog() {
 })
 }
 
-//add event listner for each button
-//once new dog buttin is clicked run rendordog function again to change the dog
-//newDogButton.addEventListener('click', () => renderDog(randomDog));//runs teh function of creating a new dog again
-
-function saveDogToFavs() {
+function saveDogToFavs() { //this adds the new dog to the save fave dogs div
     console.log('fav clicked')
     const favDog = document.querySelector("#random-dog-image > span > img") //selecting the image on the page
     const div = document.getElementById('my-fav-dogs')
     const deleteButton = document.createElement('button')
     const dogImage = document.createElement('img');
-    const span = document.createElement('div');
+    const imageDiv = document.createElement('div');
+    const buttonDiv = document.createElement('div');
     dogImage.src = favDog.src; //this gets teh specific image source of the current image
-    div.appendChild(span);
-    span.appendChild(dogImage);
-    span.appendChild(deleteButton)
+    div.appendChild(imageDiv);
+    imageDiv.appendChild(dogImage);
+    imageDiv.appendChild(buttonDiv)
+    buttonDiv.appendChild(deleteButton)
     deleteButton.innerText = 'Delete'
-    //delete button
-    //get the image
-    //get div element
-    //add image to div
 
+    
 }
